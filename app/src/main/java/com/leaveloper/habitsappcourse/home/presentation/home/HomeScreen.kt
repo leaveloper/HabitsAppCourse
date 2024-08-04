2cbd29c2c795a80316b1d7c7f90bc2912261a897
leaveloper <leaveloper@gmail.com>
2024-08-04 17:09:00 -0300

    Modal para pedir permisos

--- app/src/main/java/com/leaveloper/habitsappcourse/home/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/leaveloper/habitsappcourse/home/presentation/home/HomeScreen.kt
@@ -1,14 +1,14 @@
 package com.leaveloper.habitsappcourse.home.presentation.home
 
+import HomeAskPermission
+import android.Manifest
+import android.os.Build
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
@@ -27,7 +27,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -35,7 +34,6 @@ import com.leaveloper.habitsappcourse.R
 import com.leaveloper.habitsappcourse.home.presentation.home.components.HomeDateSelector
 import com.leaveloper.habitsappcourse.home.presentation.home.components.HomeHabit
 import com.leaveloper.habitsappcourse.home.presentation.home.components.HomeQuote
-import java.time.ZonedDateTime
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -68,6 +66,9 @@ fun HomeScreen(
                 )
             }
         }) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            HomeAskPermission(permission = Manifest.permission.POST_NOTIFICATIONS)
+        }
         LazyColumn(
             modifier = Modifier
                 .padding(it)
